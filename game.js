f1d46b49aa6472095edad147c0768787e2ccb1a4
hartoyo <zaadul.maad@gmail.com>
2025-03-27 09:39:15 +0700

    Update game.js

--- game.js
+++ game.js
@@ -35,11 +35,11 @@ window.addEventListener('resize', updateDimensions);
 
 let bird = {
     x: 50,
-    y: canvas.height    / 4,
+    y: canvas.height / 4,
     width: dimensions.width * 0.125,
     height: dimensions.height * 0.0625,
-    gravity: 0.3    ,
-    lift: -8,
+    gravity: 0.2,
+    lift: -6,
     velocity: 0
 };
 
